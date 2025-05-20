edd018441e64053d6907e078ab712fbb39077e24
George Pearse <georgehwp26@gmail.com>
2025-05-20 13:13:42 +0000

    fixed batch size and num workers

--- rfdetr/adapters/data_module.py
+++ rfdetr/adapters/data_module.py
@@ -1,19 +1,21 @@
 import lightning.pytorch as pl
+from typing import Optional
+
 import torch
 from torch.utils.data import DataLoader, DistributedSampler, RandomSampler, SequentialSampler
+import torchvision.transforms as transforms
 
 import rfdetr.util.misc as utils
 from rfdetr.adapters.dataset import CocoDetection
-from rfdetr.adapters.config import DataConfig
-import torchvision.transforms as transforms
+from rfdetr.adapters.config import DataConfig, ModelConfig
 
-def get_training_transforms(image_width: int, image_height: int):
+def get_training_transforms(image_width: int, image_height: int) -> transforms.Compose:
     return transforms.Compose([
         transforms.ToTensor(),  
         transforms.Resize((image_width, image_height)),
     ])
 
-def get_validation_transforms(image_width: int, image_height: int):
+def get_validation_transforms(image_width: int, image_height: int) -> transforms.Compose:
     return transforms.Compose([
         transforms.ToTensor(),
         transforms.Resize((image_width, image_height)),
@@ -29,17 +31,20 @@ class RFDETRDataModule(pl.LightningDataModule):
             config: Configuration as a Pydantic model or compatible dict/object
         """
         super().__init__()
-        self.num_workers = config.num_workers
-        self.training_batch_size = config.batch_size
-        self.training_num_workers = config .num_workers
-        self.training_width = config.training.training_width
-        self.training_height = config.training.training_height
+        self.num_workers = config.training_num_workers
+        self.batch_size = config.training_batch_size
+        self.training_batch_size = config.training_batch_size
+        self.training_num_workers = config.training_num_workers
+        self.validation_batch_size = config.validation_batch_size
+        self.validation_num_workers = config.validation_num_workers
+        self.training_width = config.training_width
+        self.training_height = config.training_height
     
-        self.image_directory = config.training.image_directory
-        self.training_annotation_file = config.training.training_annotation_file
-        self.validation_annotation_file = config.validation.validation_annotation_file
-        self.training_transforms = get_training_transforms(config.training.training_width, config.training.training_height)
-        self.validation_transforms = get_validation_transforms(config.validation.validation_width, config.validation.validation_height)
+        self.image_directory = config.image_directory
+        self.training_annotation_file = config.training_annotation_file
+        self.validation_annotation_file = config.validation_annotation_file
+        self.training_transforms = get_training_transforms(config.training_width, config.training_height)
+        self.validation_transforms = get_validation_transforms(config.training_width, config.training_height)
 
     def setup(self):
         """Set up datasets for training and validation."""
@@ -65,14 +70,14 @@ class RFDETRDataModule(pl.LightningDataModule):
             sampler_train = RandomSampler(self.dataset_train)
 
         batch_sampler_train = torch.utils.data.BatchSampler(
-            sampler_train, self.batch_size, drop_last=True
+            sampler_train, self.training_batch_size, drop_last=True
         )
 
         dataloader_train = DataLoader(
             self.dataset_train,
             batch_sampler=batch_sampler_train,
             collate_fn=utils.collate_fn,
-            num_workers=self.num_workers,
+            num_workers=self.training_num_workers,
         )
 
         return dataloader_train
@@ -89,11 +94,11 @@ class RFDETRDataModule(pl.LightningDataModule):
 
         dataloader_val = DataLoader(
             self.dataset_val,
-            batch_size=self.batch_size,
+            batch_size=self.validation_batch_size,
             sampler=sampler_val,
             drop_last=False,
             collate_fn=utils.collate_fn,
-            num_workers=self.num_workers,
+            num_workers=self.validation_num_workers,
         )
 
         return dataloader_val
